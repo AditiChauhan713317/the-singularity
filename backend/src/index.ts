b6b6bcee4ddd114e0fdd794f48d261e73db57630
Aditi Chauhan <chauhan28724@gmail.com>
2025-06-26 01:00:47 +0530

    deploying backend on render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
 const app = express();
+
+const allowedOrigins = [
+  'https://the-singularity-starship-to-mars.vercel.app/',
+  'http://localhost:5173' // dev
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
+
+app.use(cors());
 
 app.use(express.json());
 
